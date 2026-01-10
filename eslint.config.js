4a139510d4ce01c0ac51eed48ee01ba21f294e99
Ilham Maulana <poloklasik32@gmail.com>
2026-01-10 16:58:13 +0700

    style: standardize quotes and semicolons in config files
    - Replace double quotes with single quotes in imports
    - Add missing semicolons at end of statements
    - Simplify CI workflow by removing redundant steps

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+import storybook from 'eslint-plugin-storybook';
 
 import globals from 'globals';
 import { fixupPluginRules } from '@eslint/compat';
@@ -31,4 +31,4 @@ export default [{
       version: 'detect',
     },
   },
-}, ...storybook.configs["flat/recommended"]];
+}, ...storybook.configs['flat/recommended']];
